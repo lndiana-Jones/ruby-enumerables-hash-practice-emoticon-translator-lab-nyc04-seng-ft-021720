5cb459f19f48dd398707ce8fb7a7a9edc846b459
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-11 21:03:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,12 @@ def load_library(file)
   meaning_and_emoticons[:get_emoticon] = {} #manual creation. Need keys to be English emoticons that point to their meaning
   
   emoticons.each_pair do |pair|
-    meaning = pair[0]
+    meaning = pair[0]            #eg. 
     emoticon_array = pair[1]
-    p emoticon_array
+      if !
+    emoticon_array.each do |ele|
+      
+    end 
   end
   
   meaning_and_emoticons
