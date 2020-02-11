d66880cd806f70ad88d52b0a88740c7d2848f799
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-11 20:34:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require "yaml"
 def load_library(file)
   emoticons = YAML.load_file(file)
   meaning_and_emoticons = {}
-  meaning_and_emoticons[get_meaning] = []
-  meaning_and_emoticons[get_emoticon] = []
+  meaning_and_emoticons[:get_meaning] = []
+  meaning_and_emoticons[:get_emoticon] = []
   
   emoticons.each_pair do |pair|
     
